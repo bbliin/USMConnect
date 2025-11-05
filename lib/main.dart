223fd34c9e585808a11bc12ba7f2d369fcbf0cbf
bbliin <nicolasta34@gmail.com>
2025-11-04 22:33:30 -0300

    login terminado

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:usm_connect/pages/login_page.dart';
 import 'firebase_options.dart';
 
-// Importa el nuevo archivo que acabamos de crear
+
 import 'package:usm_connect/pages/home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializa Firebase
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6C63FF)),
         useMaterial3: true,
       ),
-      // Aquí llamamos a tu nueva HomePage
+      
       home: const LoginPage(),
     );
   }
